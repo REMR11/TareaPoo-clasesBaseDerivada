11dabfa0a7a4ce17818fef152bf390014402dbae
Ronald Mejia <74267868+REMR11@users.noreply.github.com>
2024-04-07 21:23:38 -0600

    Update Tester_Biblioteca.java

--- BibliotecaUFG/src/Biblioteca/Tester_Biblioteca.java
+++ BibliotecaUFG/src/Biblioteca/Tester_Biblioteca.java
@@ -13,12 +13,19 @@ public class Tester_Biblioteca {
 	 */
 	public static void main(String[] args) {
 
+		/**
+		 * Declaracion de variables que se ocuparan en el programa
+		 * **/
 		ArrayList<Libro> libros = new ArrayList<Libro>();
 		Biblioteca biblioteca = new Biblioteca(libros);
 		Scanner sc = new Scanner(System.in);
 		int respuesta = 0;
 
 		biblioteca.agregarLibro(sc);
+
+		/**
+		 * Obtenemos informacion sobre los libros y posibles acciones que desea realizar el usuario
+		 * **/
 		do {
 			System.out.println("Que deseas hacer ahora? \n(1) Ver listado de libros \n(2) Agregar nuevo libro"
 					+ "\n(3) Buscar un libro \n(4) Salir");
@@ -44,22 +51,16 @@ public class Tester_Biblioteca {
 				
 					List<Libro> librosEncontrados = biblioteca.buscarLibrosPorAutor(autor);
 					
-					if (librosEncontrados.isEmpty()) {
-						System.out.println("No se encontraron libros con ese autor.");
-					} else {
-						System.out.println(librosEncontrados);
-					}
+					// Llamada de metodo isEmptyBiblioteca de la clase biblioteca 
+					biblioteca.isEmptyBiblioteca(libros, "No se encontraron libros con ese autor.");
 				} else {
 					System.out.println("Proporciona titulo del libro");
 					String titulo = sc.nextLine();
 					
 					List<Libro> librosEncontrados = biblioteca.buscarLibrosPorTitulo(titulo);
 					
-					if (librosEncontrados.isEmpty()) {
-						System.out.println("No se encontraron libros con ese titulo.");
-					} else {
-						System.out.println(librosEncontrados);
-					}
+					// Llamada de metodo isEmptyBiblioteca de la clase biblioteca					
+					biblioteca.isEmptyBiblioteca(libros,"No se encontraron libros con ese titulo.");
 				}
 				break;
 			}
