08deee4fbe6b0da12b93ab325c733b0ba8fa73e4
REMR11 <ronaldmejia0211@gmail.com>
2024-04-13 08:51:29 -0600

    build: Refactorizacion de metodo UpdateProduct 	* Se decidio refactorizacion de metodo update siguiendo los principios 	  solid dado que es un requerimiento en tarea
    	* Creacion de metodo updateProdutcProperties
    	* Creacion de metodo ProdutcSales, metodo incompleto

--- storeinventory/src/main/java/com/poo/acoplamiento/storeinventory/inventary.java
+++ storeinventory/src/main/java/com/poo/acoplamiento/storeinventory/inventary.java
@@ -42,38 +42,60 @@ public class inventary {
 	 * Metodo encargado de actualizar los registros de un producto ya creado
 	 * 
 	 * @param pProducto
-	 * @return booleano que se encarga de verificar el exito del metodo
+	 * @return booleano que se encarga de verificar el exito del metodo, en caso de
+	 *         encontrar el producto y es modificado con exito devolvera true, caso
+	 *         contrario false y la excepcion que captura el error
 	 */
 	public boolean updateProduct(Producto pProducto) {
-		Producto producto = searchProduct(pProducto);
-		if (producto != null) {
-
-			int productoIndex = productos.indexOf(producto);
-
-			producto.setNombre(pProducto.getNombre());
-			producto.setPrecio(pProducto.getPrecio());
-			producto.setStock(pProducto.getStock());
-			this.productos.set(productoIndex, producto);
-
-			return true;
-		} else {
-			return false;
+		try {
+			Producto producto = searchProduct(pProducto);
+			if (producto != null) {
+				updateProductProperties(producto, pProducto);
+				return true;
+			}
+		} catch (Exception e) {
+			System.out.println("Error al actualizar el producto: " + e.getMessage());
 		}
+		return false;
+	}
+
+	/**
+	 * Metodo encargado de verificar que ninguna propiedad del producto sea nulo, en
+	 * caso de que alguna propiedad del producto que se quiere actualizar sea nulo,
+	 * se dejara el valor por defecto, asegurandonos de no tener nulos en el
+	 * producto
+	 * 
+	 * @param existingProduct
+	 * @param newProduct
+	 */
+	private void updateProductProperties(Producto existingProduct, Producto newProduct) {
+		existingProduct
+				.setNombre(newProduct.getNombre() != null ? newProduct.getNombre() : existingProduct.getNombre());
+		existingProduct
+				.setPrecio(newProduct.getPrecio() != 0.0f ? newProduct.getPrecio() : existingProduct.getPrecio());
+		existingProduct.setStock(newProduct.getStock() != 0 ? newProduct.getStock() : existingProduct.getStock());
 	}
 
 	/**
 	 * Metodo encargado de buscar productos con estado true
 	 * 
-	 * @return lista de productos disponibles
+	 * @return lista de productos disponibles, en caso de no encontrar productos
+	 *         disponibles retornara una lista vacia
+	 * 
 	 */
 	public ArrayList<Producto> getAvaibleProduct() {
 		ArrayList<Producto> avaibleProducts = new ArrayList<Producto>();
 		for (Producto producto : productos) {
-			if (producto.isEstado()) {
-				avaibleProducts.add(producto);
+			try {
+				if (producto.isEstado()) {
+					avaibleProducts.add(producto);
+				}
+			} catch (Exception e) {
+				System.out.println("Error al buscar productos disponibles: " + e.getMessage());
 			}
 		}
 		return avaibleProducts;
+
 	}
 
 	/**
@@ -102,9 +124,38 @@ public class inventary {
 	 * Metodo que se encarga de verifiacar si el codigo de producto ingresado exista
 	 * 
 	 * @param pCodigo
-	 * @return booleano que verifica su existencia
+	 * @return booleano que verifica su existencia, en caso de ser encontrado
+	 *         devolvera true, caso contrario devolvera false y la excepcion que
+	 *         captura el error
 	 */
 	public boolean containsProducto(String pCodigo) {
-		return productos.contains(new Producto(null, pCodigo, 0.0f, 0));
+		try {
+			return productos.contains(new Producto(null, pCodigo, 0.0f, 0));
+		} catch (Exception e) {
+			System.out.println("Error al verificar la existencia del producto: " + e.getMessage());
+			return false;
+		}
+	}
+
+	public void productSales(Scanner sc) {
+		ArrayList<Producto> productoLista = getAvaibleProduct();
+		int respuesta, cantidad;
+		String codigoProducto;
+		System.out.println("Lista de productos disponibles");
+		System.out.println(productoLista.toString());
+		System.out.println("Deseas comparar algo? (1)SI / (0)No");
+		respuesta = sc.nextInt();
+
+		if (respuesta == 1) {
+			System.out.println("Proporciona codigo del producto que deseas comprar");
+			codigoProducto = sc.nextLine();
+
+			if (containsProducto(codigoProducto)) {
+				System.out.println("Cuantas unidades deseas comprar?");
+				cantidad = sc.nextInt();
+
+			}
+		}
+
 	}
 }
