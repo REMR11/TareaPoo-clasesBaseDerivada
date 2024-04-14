7ce5abaf7c5650c6694c3e865fa195aa2921b632
REMR11 <ronaldmejia0211@gmail.com>
2024-04-14 16:23:01 -0600

    feat: Refactorizacion metodos en clase ventas 	* Renombramos metodo HistoriasVentas como salesHistory para 	  seguir con la costumbre de escribir todo en ingles
    	* Clase modificada Ventas, Metodo SalesHistory
    	* Creacion de metodo showSales

--- storeinventory/src/main/java/com/poo/acoplamiento/storeinventory/Ventas.java
+++ storeinventory/src/main/java/com/poo/acoplamiento/storeinventory/Ventas.java
@@ -6,52 +6,74 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * Clase ventas implementa la logica necesaria para manipular la clase inventary
+ * {@link inventary}
+ * 
+ * @author remr1
+ * 
+ * 
+ */
 public class Ventas {
+	// Declaracion de variables globales utilizadas en la clases
 	public List<String> venta;
 	public inventary inventarioproducto = new inventary();
 
-	public void historialVentas(Scanner sc) {
+	/**
+	 * Metodo encargado de capturas datos sobre las ventas de producto, cada venta
+	 * es ingresada en un lista
+	 * 
+	 * @param sc
+	 * 
+	 * @throws NullPointerException   Si el valor ingresado es nulo
+	 * @throws InputMismatchException si el valor ingresado no es valido
+	 */
+	public void salesHistory(Scanner sc) {
 		int respuesta, cantidad = 0;
 		String codigoProducto = "";
 
 		Producto auxProducto = new Producto();
-		System.out.println("Deseas comprar algo? (1)SI / (0)No");
+//		System.out.println("Deseas comprar algo? (1)SI / (0)No");
+//		respuesta = sc.nextInt();
+//		
+//		if (respuesta == 1) {}
 		try {
-			respuesta = sc.nextInt();
-			
-			if (respuesta == 1) {
-				System.out.println("Proporciona codigo del producto que deseas comprar");
-			
-				try {
-				
-					codigoProducto = sc.nextLine();
-					inventary auxInventary = new inventary();
-					auxProducto = auxInventary.searchProduct(codigoProducto);
-				
-				} catch (NullPointerException e) {
-					
-					System.out.println("Error: No se proporciono un codigo de producto");
-				
-				}
-				
-				System.out.println("Cuantas unidades deseas comprar?");
-				
-				try {
-				
-					cantidad = sc.nextInt();
-				
-				} catch (InputMismatchException e) {
-				
-					System.out.println("Error: Ingrese un numero valido");
+			System.out.println("Proporciona codigo del producto que deseas comprar");
 
-				}
-				venta.add(codigoProducto + ", " + cantidad + ", " + auxProducto.getPrecio()+ "\n");
-				inventarioproducto.productSales(codigoProducto, cantidad);
+			try {
+
+				codigoProducto = sc.nextLine();
+				inventary auxInventary = new inventary();
+				auxProducto = auxInventary.searchProduct(codigoProducto);
+
+			} catch (NullPointerException e) {
+
+				System.out.println("Error: No se proporciono un codigo de producto");
+
 			}
+
+			System.out.println("Cuantas unidades deseas comprar?");
+
+			try {
+
+				cantidad = sc.nextInt();
+
+			} catch (InputMismatchException e) {
+
+				System.out.println("Error: Ingrese un numero valido");
+
+			}
+			venta.add(codigoProducto + ", " + cantidad + ", " + auxProducto.getPrecio() + "\n");
+			inventarioproducto.productSales(codigoProducto, cantidad);
+
 		} catch (InputMismatchException e) {
 			System.out.println("Error: Ingrese un numero valido");
 
 		}
 
+	}
+
+	public void showSales() {
+		System.out.println(venta);
 	}
 }
